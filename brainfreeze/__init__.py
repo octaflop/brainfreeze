e9ee7eb82ca530d3a9df284d1478000e8b6f8660
Faris Chebib <faris@zaneprep.com>
2013-07-23 20:01:07 -0600

    building out templates

--- brainfreeze/__init__.py
+++ brainfreeze/__init__.py
@@ -1,5 +1,10 @@
-from flask import Flask, request
+import sys
+# Static flask imports
+from flask import Flask, render_template
+# Dynamic flask imports
+from flask import request
 from flask_flatpages import FlatPages
+from flask_frozen import Freezer
 
 DEBUG = True
 FLATPAGES_AUTO_RELOAD = DEBUG
@@ -8,14 +13,19 @@ FLATPAGES_EXTENSION = '.md'
 app = Flask(__name__)
 app.config.from_object(__name__)
 pages = FlatPages(app)
+freezer = Freezer(app)
 
 @app.route("/")
 def index():
-    return "Hola %s" % request.accept_mimetypes
+    # return "Hola %s" % request.accept_mimetypes
+    return render_template("index.html", page=page)
 
 @app.route('/<path:path>')
 def page(path):
-    return pages.get_or_404(path).html
+    page = pages.get_or_404(path).html
+    return render_template("page.html", page=page)
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    if len(sys.argv) > 1 and sys.argv[1] == "build":
+        freezer.freeze()
+    app.run(port=5000, debug=DEBUG)
