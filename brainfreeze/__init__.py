3dc85a2a66bc896906ccce4c3411198e13ef41cb
Faris Chebib <faris@zaneprep.com>
2013-07-24 22:46:15 -0600

    initial static/css

--- brainfreeze/__init__.py
+++ brainfreeze/__init__.py
@@ -22,7 +22,7 @@ def index():
 
 @app.route('/<path:path>')
 def page(path):
-    page = pages.get_or_404(path).html
+    page = pages.get_or_404(path)
     return render_template("page.html", page=page)
 
 if __name__ == "__main__":
